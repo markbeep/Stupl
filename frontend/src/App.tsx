6e8b7d6f9d0da382874ac9cefa47f76f9687cc87
Krecharles <charelctk@gmail.com>
2022-10-13 10:35:52 +0200

    Add Routing and css cleanup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
 import TestHome from "./pages/test-home";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
-    <div>
-      <TestHome />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<TestHome />}></Route>
+        <Route path="/example" element={<TestHome />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/pages/test-home.tsx
+++ frontend/src/pages/test-home.tsx
@@ -1,13 +1,18 @@
-import SampleCard from '../components/sample-card';
+import React from "react";
+import { Navbar } from "../components/Navbar";
+import SampleCard from "../components/sample-card";
 
 const TestHome = () => {
-    return (
-        <div>
-            <h1>Nothing much here</h1>
-            <SampleCard />
-            <button className='btn'>Here</button>
-        </div>
-    );
+  return (
+    <React.Fragment>
+      <Navbar></Navbar>
+      <div className="container mx-auto">
+        <h1>Nothing much here</h1>
+        <SampleCard />
+        <button className="btn">Here</button>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default TestHome;
