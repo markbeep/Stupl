b273e43a8265e5a88eb1d62858e339166ec62a9d
Konstantin <konstantin.sturm@gmail.com>
2022-10-14 21:50:44 +0200

    Konsti seite

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import TestHome from "./pages/test-home";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
+import { useState } from "react";
 
 function App() {
   return (
@@ -8,9 +9,16 @@ function App() {
       <Routes>
         <Route path="/" element={<TestHome />}></Route>
         <Route path="/example" element={<TestHome />}></Route>
+        <Route path="/konsti" element={<Konsti />}></Route>
       </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
+
+const Konsti = () => {
+  const [counter, setCounter] = useState(0);
+
+  return <div>{counter} </div>;
+};
