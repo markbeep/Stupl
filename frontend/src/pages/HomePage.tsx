c7a333fb92fd58bcb29d643f87dc0c0edac248e7
Konstantin <konstantin.sturm@gmail.com>
2022-10-15 18:01:53 +0200

    Make animation for edit button

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -222,6 +222,50 @@ const totalWsum = ({
   }
 };
 
+//Edit button
+const DisplaySubjects = ({ subject }: { subject: SubjectData }) => {
+  const [showEditButton, setShowEditButton] = useState(false);
+  return (
+    <tr
+      onMouseEnter={() => setShowEditButton(true)}
+      onMouseLeave={() => setShowEditButton(false)}
+      key={subject.id}
+    >
+      <td className="text-left pr-2">{subject.name}</td>
+      <td className="pr-2">
+        <SemesterPill semester={subject.semester}></SemesterPill>
+      </td>
+      <td className="text-right pr-2">{subject.ects}</td>
+      <td className="text-right pr-2">{subject.grade}</td>
+      {/* was macht das hover hier*/}
+
+      <td
+        className={
+          "transition-all " + (showEditButton ? " opacity-100" : " opacity-0")
+        }
+      >
+        <button className="" onClick={() => console.log("Suck my nuts")}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="feather feather-edit"
+          >
+            <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
+            <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
+          </svg>
+        </button>
+      </td>
+    </tr>
+  );
+};
+
 const SubjectGroupCollapsible = ({
   subjectGroup,
   includePlanned,
@@ -275,36 +319,7 @@ const SubjectGroupCollapsible = ({
         </thead>
         <tbody>
           {subjectGroup.data.map((subject) => (
-            <tr key={subject.id}>
-              <td className="text-left pr-2">{subject.name}</td>
-              <td className="pr-2">
-                <SemesterPill semester={subject.semester}></SemesterPill>
-              </td>
-              <td className="text-right pr-2">{subject.ects}</td>
-              <td className="text-right pr-2">{subject.grade}</td>
-              <td className="transition-all  hover:opacity-100">
-                <button
-                  className=""
-                  onClick={() => console.log("Suck my nuts")}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="feather feather-edit"
-                  >
-                    <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
-                    <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
-                  </svg>
-                </button>
-              </td>
-            </tr>
+            <DisplaySubjects subject={subject}></DisplaySubjects>
           ))}
           <tr className="">
             <td className="text-left pr-2 bg-base-200">Total</td>
--- frontend/src/pages/konsti.tsx
+++ frontend/src/pages/konsti.tsx
@@ -1,59 +1,275 @@
 import { useState } from "react";
+import Collapsible from "../components/Collapsible";
+import { Navbar } from "../components/Navbar";
+import SearchBar from "../components/SearchBar";
+import SemesterPill from "../components/SemesterPill";
+import { SubjectGroup, subjectGroups, SubjectData } from "../data";
+import { RequirementsCollapsible } from "./Yanick";
 
 export const Konsti = () => {
+  const [includePlanned, setIncludePlanned] = useState(false);
+
+  const el = document.getElementById("box");
+
+  const hiddenEl = document.getElementById("hidden");
+
   return (
-    <div className="tooltip mt-10" data-tip="hello">
-      <button className="btn" mt-10>
-        Hover me
-      </button>
+    <div className="pb-12">
+      <Navbar></Navbar>
+
+      <div className="mt-12 max-w-lg mx-auto">
+        <SearchBar></SearchBar>
+      </div>
+      <div className="max-w-2xl mx-auto mt-24">
+        <div className="alert alert-">
+          <div className="flex justify-between w-full">
+            <h3 className="font-bold">Total</h3>
+            <div className="flex">
+              <p className="mr-8">37/54</p>
+              <p className="mr-10">5.37</p>
+            </div>
+          </div>
+        </div>
+        {subjectGroups.map((group) => (
+          <div className="mt-12">
+            <SubjectGroupCollapsible
+              subjectGroup={group}
+              includePlanned={includePlanned}
+            ></SubjectGroupCollapsible>
+          </div>
+        ))}
+        <div className="mt-12">
+          <RequirementsCollapsible></RequirementsCollapsible>
+        </div>
+      </div>
     </div>
   );
 };
 
-const SearchBar = () => {
+export default Konsti;
+
+const showSubject = ({
+  includePlanned,
+  subjectTableRowData,
+}: {
+  includePlanned: boolean;
+  subjectTableRowData: SubjectData;
+}) => {
+  return includePlanned ? true : subjectTableRowData.planned ? false : true;
+};
+
+//compute sum of ECTS per category (that are to be included based on includePlanned)
+const sumEcts = ({
+  includePlanned,
+  subjectGroup,
+}: {
+  includePlanned: boolean;
+  subjectGroup: SubjectGroup;
+}) => {
+  return subjectGroup.data
+    .reduce((accumulator, currentValue) => {
+      return (
+        accumulator +
+        (showSubject({
+          includePlanned: includePlanned,
+          subjectTableRowData: currentValue,
+        })
+          ? currentValue.ects
+          : 0)
+      );
+    }, 0)
+    .toFixed(2);
+};
+
+//compute the weigthed sum of grades * ECTS per category
+const wsumGrades = ({
+  includePlanned,
+  subjectGroup,
+}: {
+  includePlanned: boolean;
+  subjectGroup: SubjectGroup;
+}) => {
+  console.log(subjectGroup.data);
+  return subjectGroup.data.reduce((accumulator, currentValue) => {
+    return (
+      accumulator +
+      (showSubject({
+        includePlanned: includePlanned,
+        subjectTableRowData: currentValue,
+      })
+        ? currentValue.grade * currentValue.ects
+        : 0)
+    );
+  }, 0);
+};
+
+//compute the average grade based one includePlanned per category
+const avgGrades = ({
+  includePlanned,
+  subjectGroup,
+}: {
+  includePlanned: boolean;
+  subjectGroup: SubjectGroup;
+}) => {
+  console.log(
+    Number(
+      sumEcts({
+        includePlanned: includePlanned,
+        subjectGroup: subjectGroup,
+      })
+    )
+  );
+  if (
+    Number(
+      sumEcts({
+        includePlanned: includePlanned,
+        subjectGroup: subjectGroup,
+      })
+    ) === 0
+  ) {
+    return 0;
+  } else {
+    return (
+      Number(
+        wsumGrades({
+          includePlanned: includePlanned,
+          subjectGroup: subjectGroup,
+        })
+      ) /
+      Number(
+        sumEcts({
+          includePlanned: includePlanned,
+          subjectGroup: subjectGroup,
+        })
+      )
+    ).toFixed(2);
+  }
+};
+//function
+const DisplaySubjects = ({ subject }: { subject: SubjectData }) => {
+  const [showEditButton, setShowEditButton] = useState(false);
   return (
-    <input type="text" placeholder="Search" className="input w-full max-w-xs" />
+    <tr
+      onMouseEnter={() => setShowEditButton(true)}
+      onMouseLeave={() => setShowEditButton(false)}
+      key={subject.id}
+    >
+      <td className="text-left pr-2">{subject.name}</td>
+      <td className="pr-2">
+        <SemesterPill semester={subject.semester}></SemesterPill>
+      </td>
+      <td className="text-right pr-2">{subject.ects}</td>
+      <td className="text-right pr-2">{subject.grade}</td>
+      {/* was macht das hover hier*/}
+
+      <td
+        className={
+          "transition-all " + (showEditButton ? " opacity-100" : " opacity-0")
+        }
+      >
+        <button className="" onClick={() => console.log("Suck my nuts")}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="feather feather-edit"
+          >
+            <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
+            <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
+          </svg>
+        </button>
+      </td>
+    </tr>
   );
 };
 
-export const SearchBar2 = () => {
-  const [searchInput, setSearchInput] = useState(0);
-
-  const countries = [
-    { name: "Belgium", continent: "Europe" },
-    { name: "India", continent: "Asia" },
-    { name: "Bolivia", continent: "South America" },
-    { name: "Ghana", continent: "Africa" },
-    { name: "Japan", continent: "Asia" },
-    { name: "Canada", continent: "North America" },
-    { name: "New Zealand", continent: "Australasia" },
-    { name: "Italy", continent: "Europe" },
-    { name: "South Africa", continent: "Africa" },
-    { name: "China", continent: "Asia" },
-    { name: "Paraguay", continent: "South America" },
-    { name: "Usa", continent: "North America" },
-    { name: "France", continent: "Europe" },
-    { name: "Botswana", continent: "Africa" },
-    { name: "Spain", continent: "Europe" },
-    { name: "Senegal", continent: "Africa" },
-    { name: "Brazil", continent: "South America" },
-    { name: "Denmark", continent: "Europe" },
-    { name: "Mexico", continent: "South America" },
-    { name: "Australia", continent: "Australasia" },
-    { name: "Tanzania", continent: "Africa" },
-    { name: "Bangladesh", continent: "Asia" },
-    { name: "Portugal", continent: "Europe" },
-    { name: "Pakistan", continent: "Asia" },
-  ];
-
+const SubjectGroupCollapsible = ({
+  subjectGroup,
+  includePlanned,
+}: {
+  subjectGroup: SubjectGroup;
+  includePlanned: boolean;
+}) => {
+  const [hideLightbox, setHideLightbox] = useState(true);
   return (
-    <>
-      {countries.map(({ name, continent }) => (
-        <p key={name}>
-          {" "}
-          {name} {continent}{" "}
-        </p>
-      ))}
-    </>
+    <Collapsible
+      headerBuilder={(collapsed) => (
+        <div className="flex justify-between">
+          <div className="tooltip" data-tip={subjectGroup.information}>
+            <h3>{subjectGroup.name}</h3>
+          </div>
+          <div className="flex">
+            {collapsed && (
+              <p className="mr-8">
+                {Number(
+                  sumEcts({
+                    includePlanned: includePlanned,
+                    subjectGroup: subjectGroup,
+                  })
+                )}
+                /54
+              </p>
+            )}
+            {collapsed && (
+              <p className="mr-2">
+                {" "}
+                {Number(
+                  avgGrades({
+                    includePlanned: includePlanned,
+                    subjectGroup: subjectGroup,
+                  })
+                )}
+              </p>
+            )}
+          </div>
+        </div>
+      )}
+    >
+      <table className="table w-full">
+        <thead className="rounded-none">
+          <tr className="rounded-none">
+            <th className="text-left z-index0-force"></th>
+            <th className="pr-2">Semester</th>
+            <th className="pr-2">ECTS</th>
+            <th className="pr-2">Grade</th>
+            <th className=""></th>
+          </tr>
+        </thead>
+        <tbody>
+          {subjectGroup.data.map((subject) => (
+            <DisplaySubjects subject={subject}></DisplaySubjects>
+          ))}
+          <tr className="">
+            <td className="text-left pr-2 bg-base-200">Total</td>
+            <td className="bg-base-200 pr-2"></td>
+            <td className="text-right bg-base-200 pr-2">
+              {Number(
+                sumEcts({
+                  includePlanned: includePlanned,
+                  subjectGroup: subjectGroup,
+                })
+              )}
+              /54
+            </td>
+            {/* compute average grade per component */}
+            <td className="text-right bg-base-200 pr-2">
+              {Number(
+                avgGrades({
+                  includePlanned: includePlanned,
+                  subjectGroup: subjectGroup,
+                })
+              )}
+            </td>
+            <td className="bg-base-200 "></td>
+          </tr>
+        </tbody>
+      </table>
+    </Collapsible>
   );
 };
