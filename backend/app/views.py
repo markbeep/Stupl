5e1848705836518882bd019c9c5a45344a26ed7c
Leander Diaz-Bone <leanderdiazbone@gmail.com>
2022-10-15 19:54:24 +0200

    Worked on APIs.

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,4 +1,5 @@
 
+from unicodedata import category
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -84,7 +85,31 @@ def delete_subject(request):
     subject.delete()
     return Response("Success")
 
+def sumCreditsCategories(user, categoryList):
+    sum = 0
+    for cat in categoryList:
+        credits = 0
+        for sub in UserSubjects.objects.filter(user=user, category=cat):
+            credits = credits + sub.credits
+        sum = sum + credits
+    return sum
 
+@api_view(["GET"])
+def requirements(request):
+    user = request.user
+    return Response({
+        {
+            "1": sumCreditsCategories(user, []) >= 100,
+            "2": True,
+            "3": True,
+            "4": True,
+            "5": True,
+            "6": True,
+            "7": True,
+            "8": True,
+            "9": True,
+        }
+    })
 
 @api_view(["GET"])
 def fill_db(request):
