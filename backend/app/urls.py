01b7f06ecb3eae251df43537b5f0144513193451
Mark Csurgay <mcsurgay@student.ethz.ch>
2022-10-15 18:49:53 +0200

    urls

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.list_temporary),
-    path("add/",views.add_subject),
-    path("vvz/",views.load_vvz),
-    path("delete/",views.delete_subject),
+    path("list_subjects/", views.list_temporary),
+    path("add_subject/",views.add_subject),
+    path("load_vvz/",views.load_vvz),
+    path("delete_subject/",views.delete_subject),
     path("hello/", views.list_temporary),       
     path("fill_db/", views.fill_db),    
-    path("get/", views.get_subjects_per_user)   
+    path("get_subjects/", views.get_subjects_per_user)   
 ]
