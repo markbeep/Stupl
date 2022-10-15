dc3a397e1cbe360229bde819962d882a9bf55200
Konstantin <konstantin.sturm@gmail.com>
2022-10-15 19:27:08 +0200

    statistics page and about page added

---
+++ frontend/src/pages/StatisticsPage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Navbar } from "../components/Navbar";
+
+type Props = {};
+
+const StatisticsPage = (props: Props) => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div>StatisticsPage</div>
+    </div>
+  );
+};
+
+export default StatisticsPage;
