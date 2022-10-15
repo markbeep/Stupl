6d870658e0ae97f34215928ed84477ee4dd478b5
Krecharles <charelctk@gmail.com>
2022-10-15 16:09:42 +0200

    up

--- frontend/src/api/hooks.tsx
+++ frontend/src/api/hooks.tsx
@@ -38,9 +38,12 @@ export async function loginUser(email: string, password: string) {
   }
 }
 
-async function loadTestHello() {
+export async function loadTestHello() {
   const response = await fetch("/api/hello");
+  console.log(response);
+  console.log(response.body);
   const data = await response.json();
+  console.log("why syntax error", response);
   return { word: data } as TestWord;
 }
 export function useTestHello() {
@@ -52,4 +55,24 @@ export const createSubject = async (s: SubjectData) => {
   await new Promise((resolve) => setTimeout(resolve, 500));
   console.log(s);
   return { success: true };
+};
+
+export const registerUser = async (email: string, password: string) => {
+  console.log(email, password);
+  const options = {
+    method: "POST",
+    body: JSON.stringify({ email, password }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  const response = await fetch("/auth/register/", options);
+  console.log("test");
+  console.log(response);
+  if (response.status == 200) {
+    const data = await response.json();
+    return data["token"] as string;
+  } else {
+    return "invalid login";
+  }
 };
