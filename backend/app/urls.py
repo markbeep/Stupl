18cab97263128ed38ee661f30e27b264da8a6d3e
Leander Diaz-Bone <leanderdiazbone@gmail.com>
2022-10-15 09:08:39 +0200

    Worked on APIs.

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.list_temporary),
     path("<int:pk>/", views.get_page),        
-    path("test/", views.load_main_info),        
+    path("main/", views.load_main_info),
+    path("add/",views.add_subject),
+    path("delete/",views.del_subject)
 ]
--- backend/app/views.py
+++ backend/app/views.py
@@ -23,22 +23,21 @@ def get_page(request, pk: int):
 def load_main_info(request):
     # From UserSubjects return list with all information for certain user
     user = request.user
-    
-    subs = UserSubjects.objects.all().filter(user=username)
-    return subs
+    #subs = UserSubjects.objects.all().filter(user=user)
+    return JsonResponse({"name": "Linear Algebra", "credits": "7\u00a0credits", "id": 163637, "sem": False, "category": "First Year Examinations"})
 
 @api_view(["POST"])
 def add_subject(request):
     # Add subject to UserSubjects
-    user = UserSubjects.objects.create()
-    user.name = request.POST["name"]
-    user.credits = request.POST["credits"]
-    user.category = request.POST["category"]
-    user.semester = request.POST["semester"]
-    user.grade = request.POST["grade"]
-    user.count_grade = True
-    user.count_credits = True
-    return
+    #user = UserSubjects.objects.create()
+    #user.name = request.POST["name"]
+    #user.credits = request.POST["credits"]
+    #user.category = request.POST["category"]
+    #user.semester = request.POST["semester"]
+    #user.grade = request.POST["grade"]
+    #user.count_grade = True
+    #user.count_credits = True
+    return JsonResponse(request)
 
 def del_subject(request):
     # Delete subject from UserSubjects
