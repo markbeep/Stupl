337b9443c1f56e31766a8d14e7060e0a64a23e8f
yschimpf <yaschimpf@gmail.com>
2022-10-15 23:56:02 +0200

    anderungen

---
+++ frontend/src/pages/Stats.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Navbar } from "../components/Navbar";
+
+type Props = {};
+
+const StatsPage = (props: Props) => {
+  return (
+    <div>
+      <Navbar></Navbar>
+    </div>
+  );
+};
+
+export default StatsPage;
