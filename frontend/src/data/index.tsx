3e956b75173dbddd86c09ea66f0d6a8bec106c42
Konstantin <konstantin.sturm@gmail.com>
2022-10-15 14:15:07 +0200

    Add hover

--- frontend/src/data/index.tsx
+++ frontend/src/data/index.tsx
@@ -52,11 +52,25 @@ export const subjectGroups: SubjectGroup[] = [
   {
     name: "Basisjahr",
     data: subjectTableRowData,
-    information: "Kacke",
+    information:
+      "Courses usually done in first year They're split into two block exams. (All other courses have to be passed individually)",
   },
   {
     name: "Grundlagenfächer",
     data: subjectTableRowData,
-    information: "noch mehr kacke",
+    information:
+      "Courses usually done in second year. Requirements: All courses need to be taken, but only seven of eight have to be passed. If you don't pass all, you need to take an additional core subject instead.",
+  },
+  {
+    name: "Kernfächer",
+    data: subjectTableRowData,
+    information:
+      "Courses usually done in second year. Requirements: At least four courses need to be taken. You can also take more.",
+  },
+  {
+    name: "Gess",
+    data: subjectTableRowData,
+    information:
+      "Courses"
   },
 ];
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -45,7 +45,9 @@ const SubjectGroupCollapsible = ({
     <Collapsible
       headerBuilder={(collapsed) => (
         <div className="flex justify-between">
-          <h3>{subjectGroup.name}</h3>
+          <div className="tooltip" data-tip={subjectGroup.information}>
+            <h3>{subjectGroup.name}</h3>
+          </div>
           <div className="flex">
             {collapsed && <p className="mr-8">37/54</p>}
             {collapsed && <p className="mr-2">5.37</p>}
