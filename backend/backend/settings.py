ddae1d2831529b8a660c2a07f5091f2b74b9e95d
Mark <mcsurgay@student.ethz.ch>
2022-10-27 08:49:39 +0200

    webhook test #3

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
+Hello There
 """
 
 from pathlib import Path
